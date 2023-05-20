84aaa6587fec63bba53fd6fcd6e7c06bf59e2a3c
kmlaird <kmlaird15@gmail.com>
2023-05-19 20:21:53 -0700

    Initial current weather section setup

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "./CurrentWeather.css";
 
 export default function CurrentWeather(props) {
   return (
     <div className="cw">
-      <p>Test</p>
+      <h2>Currently in city</h2>
+      <div className="row ctemp">
+        <div className="col ct">
+          <div className="row">
+            <div className="col">
+              <p>icon</p>
+              <p>description</p>
+            </div>
+            <div className="col">
+              <p>icon</p>
+              <p>description</p>
+            </div>
+          </div>
+        </div>
+        <div className="col ct">
+          <p>Humidity</p>
+          <p>Wind</p>
+          <p>visibility</p>
+        </div>
+        <div className="col ct">
+          <p>sunrise</p>
+          <p>sunset</p>
+        </div>
+      </div>
     </div>
   );
 }
